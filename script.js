2b51002b030efdace01444f2083c9166d223a9ed
Jamie Flynt <jamflynt@gmail.com>
2021-08-28 15:18:11 -0400

    Created code along simple Javascript game from Youtube user KnifeCircus. Added orginal 'characters.'

---
+++ script.js
@@ -0,0 +1,21 @@
+var character = 
+document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump(){
+    if(character.classList !="animate"){
+    character.classList.add("animate");
+}
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+}
+
+var checkDead = setInterval (function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<50 && blockLeft>0 && characterTop>=400){
+        block.style.animation = "none";
+        alert("U LOSE SUCKER");
+    }
+}, 10);
